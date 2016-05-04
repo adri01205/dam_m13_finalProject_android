edf7723102c22ac480b92a8f62f7ef9a42547815
adri01205 <adri01205@gmail.com>
2016-05-04 13:48:13 +0200

    android project update

---
+++ dam_m13_finalProject_android/app/src/test/java/com/m13/dam/dam_m13_finalproject_android/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.m13.dam.dam_m13_finalproject_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
