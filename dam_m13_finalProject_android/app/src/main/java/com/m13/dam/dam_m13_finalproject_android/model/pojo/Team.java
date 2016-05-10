6bafd1867121dc03d1ca8efe7eb62ef8fe85cf72
jesus <susocorleone@gmail.com>
2016-05-10 18:44:25 +0200

    Team Table  Added

---
+++ dam_m13_finalProject_android/app/src/main/java/com/m13/dam/dam_m13_finalproject_android/model/pojo/Team.java
@@ -0,0 +1,35 @@
+package com.m13.dam.dam_m13_finalproject_android.model.pojo;
+
+/**
+ * Created by jesus on 09/05/2016.
+ */
+public class Team
+{
+
+    private String code;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
