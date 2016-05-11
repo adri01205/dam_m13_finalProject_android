395f84cf090a9591813c1a96816c97b12a41d1b2
suso01 <susocorleone@gmail.com>
2016-05-11 15:38:36 +0200

    Last modified through centralized model

--- dam_m13_finalProject_android/app/src/main/java/com/m13/dam/dam_m13_finalproject_android/model/pojo/Last.java
+++ dam_m13_finalProject_android/app/src/main/java/com/m13/dam/dam_m13_finalproject_android/model/pojo/Last.java
@@ -3,27 +3,36 @@ package com.m13.dam.dam_m13_finalproject_android.model.pojo;
 /**
  * Created by adri on 04/05/2016.
  */
+
+/**
+ *  Modified by jesus on 10/06/2016
+ *
+ *  Added 'teamLog' field
+ */
 public class Last {
 
     int id;
     int employeeLog;
     int taskLog;
     int employeTaskLog;
+    int teamLog;
 
     public Last() {
     }
 
-    public Last(int id, int employeeLog, int taskLog, int employeTaskLog) {
+    public Last(int id, int employeeLog, int taskLog, int employeTaskLog, int teamLog) {
         this.id = id;
         this.employeeLog = employeeLog;
         this.taskLog = taskLog;
         this.employeTaskLog = employeTaskLog;
+        this.teamLog = teamLog;
     }
 
-    public Last(int employeeLog, int taskLog, int employeTaskLog) {
+    public Last(int employeeLog, int taskLog, int employeTaskLog, int teamLog) {
         this.employeeLog = employeeLog;
         this.taskLog = taskLog;
         this.employeTaskLog = employeTaskLog;
+        this.teamLog = teamLog;
     }
 
     public int getId() {
@@ -58,6 +67,14 @@ public class Last {
         this.employeTaskLog = employeTaskLog;
     }
 
+    public int getTeamLog() {
+        return teamLog;
+    }
+
+    public void setTeamLog(int teamLog) {
+        this.teamLog = teamLog;
+    }
+
     @Override
     public String toString() {
         return "Last{" +
@@ -65,6 +82,7 @@ public class Last {
                 ", employeeLog=" + employeeLog +
                 ", taskLog=" + taskLog +
                 ", employeTaskLog=" + employeTaskLog +
+                ", teamLog=" + teamLog +
                 '}';
     }
 }
