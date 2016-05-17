07b4ef89e6578d66923fb37a03c27709d6c97525
jesus <susocorleone@gmail.com>
2016-05-17 13:46:38 +0200

    expandable adapterl

--- dam_m13_finalProject_android/app/src/main/java/com/m13/dam/dam_m13_finalproject_android/controller/TaskListActivity.java
+++ dam_m13_finalProject_android/app/src/main/java/com/m13/dam/dam_m13_finalproject_android/controller/TaskListActivity.java
@@ -1,24 +1,74 @@
 package com.m13.dam.dam_m13_finalproject_android.controller;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ExpandableListView;
+
+import com.m13.dam.dam_m13_finalproject_android.R;
+import com.m13.dam.dam_m13_finalproject_android.controller.adapters.TeamTaskAdapter;
+import com.m13.dam.dam_m13_finalproject_android.model.dao.SynupConversor;
+import com.m13.dam.dam_m13_finalproject_android.model.dao.SynupSharedPreferences;
+import com.m13.dam.dam_m13_finalproject_android.model.pojo.Task;
+import com.m13.dam.dam_m13_finalproject_android.model.pojo.Team;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by jesus on 10/05/2016.
  */
-public class TaskListActivity extends AppCompatActivity
+
+public class TaskListActivity extends SynupMenuActivity
         implements ExpandableListView.OnGroupClickListener, ExpandableListView.OnChildClickListener {
+
+
+    ArrayList<Team> teams;
+    HashMap<Team, ArrayList<Task>> tasksMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.tasks_adapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        chargeData();
+
+        ExpandableListView elv = (ExpandableListView)findViewById(R.id.lstLlistaExpandible);
+        TeamTaskAdapter adaptador = new TeamTaskAdapter(this, teams, tasksMap);
 
 
     }
 
+    public void chargeData()
+    {
+        teams = new ArrayList<>();
+        tasksMap = new HashMap<>();
+        SynupConversor syn = new SynupConversor(this);
+        Cursor cTeams =  syn.getTeamsByUser(SynupSharedPreferences.getUserLoged(this));
+
+        if (cTeams.moveToFirst()){
+            do{
+                Team team = (Team) cTeams;
+                teams.add(team);
+                ArrayList<Task> teamTasks = new ArrayList<>();
+
+                Cursor tasks = syn.getTaskByTeam(team.getCode());
+
+                if (tasks.moveToFirst()){
+                    do{
+                        teamTasks.add((Task) tasks);
+                    }while(tasks.moveToFirst());
+                }
+
+                tasksMap.put(team, teamTasks);
+            } while (cTeams.moveToNext ());
+        }
+    }
 
 
     @Override
